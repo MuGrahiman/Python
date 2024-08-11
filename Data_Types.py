e7613b46d72c78cd51a60b303cd1a026c8b898bd
Mujeeb Rahiman <mujeebrahiman2000@gmail.com>
2024-08-11 10:58:55 +0530

    analyze:Data Type

---
+++ Data_Types.py
@@ -0,0 +1,25 @@
+# PYTHON DATA TYPES 
+
+#* In programming, data type is an important concept.
+#* Variables can store data of different types.
+
+# FINDING DATA TYPE 
+type()#input any data . this will return the type of the data
+
+# | Example                                         | Data Type  | How to Specify  | Category   |
+# |-------------------------------------------------|------------|-----------------|------------|
+# | x = "Hello World"                               | str        | str()           | Text       |
+# | x = 20                                          | int        | int()           | Numeric    |
+# | x = 20.5                                        | float      | float()         | Numeric    |
+# | x = 1j                                          | complex    | complex()       | Numeric    |
+# | x = ["apple", "banana", "cherry"]               | list       | list()          | Sequence   |
+# | x = ("apple", "banana", "cherry")               | tuple      | tuple()         | Sequence   |
+# | x = range(6)                                    | range      | range()         | Sequence   |
+# | x = {"name" : "John", "age" : 36}               | dict       | dict()          | Mapping    |
+# | x = {"apple", "banana", "cherry"}               | set        | set()           | Set        |
+# | x = frozenset({"apple", "banana", "cherry"})    | frozenset  | frozenset()     | Set        |
+# | x = True                                        | bool       | bool()          | Boolean    |
+# | x = b"Hello"                                    | bytes      | bytes()         | Binary     |
+# | x = bytearray(5)                                | bytearray  | bytearray()     | Binary     |
+# | x = memoryview(bytes(5))                        | memoryview | memoryview()    | Binary     |
+# | x = None                                        | None       | None            | None       |
