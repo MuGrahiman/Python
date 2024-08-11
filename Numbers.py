0c0e7f1ac91d1410dc5fe46bf56f7aa2674061f6
Mujeeb Rahiman <mujeebrahiman2000@gmail.com>
2024-08-11 10:05:00 +0530

    analyze:Numbers

---
+++ Numbers.py
@@ -0,0 +1,21 @@
+# Python have 3 numeric types int , float , complex 
+
+x = 1    # int :is a whole number, positive or negative, without decimals, of unlimited length
+
+y = 2.8  # float :is a number, positive or negative, containing one or more decimals.
+
+z = 1j   # complex :is a numbers that have both a real part and an imaginary part.
+         # j represents the square root of -1 
+         # Complex numbers in Python are a useful data type for applications that involve mathematical      calculations, signal processing, control systems, and more.
+         
+
+# TYPE 
+#  Checking     Convertion
+#* type()       int()
+#*              float() 
+#*              complex()
+
+# RANDOM :Python built-in module used to make a random number
+# Eg :
+# import random
+# print(random.randrange(1, 10))
